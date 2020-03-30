6cf1615f58145b6391491c325f078f2e92ecfa4b
Maxi-F <maxifeldman@hotmail.com>
2020-03-30 11:43:53 -0300

    Started user registration POST

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route   POST api/users
-// @desc    Test route
+// @desc    Register user
 // @access  Public
-router.post('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email is not a valid one').isEmail(),
+    check(
+      'password',
+      'please enter a password with 8 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log(req.body);
+    res.send('asd');
+  }
+);
 
 module.exports = router;
