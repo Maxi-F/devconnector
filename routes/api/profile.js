bcf384679ad11db55586d81f621e10736b3370d1
Maxi-F <maxifeldman@hotmail.com>
2020-03-31 14:09:50 -0300

    finished profiles, and getting repos from github

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const axios = require('axios');
+const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -119,12 +121,127 @@ router.put(
     check('location', 'Location is required').notEmpty()
   ],
   (req, res) => {
-    tryOrServerError(res, async () => {
-      const experience = new Profile(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-      res.json({ msg: 'User deleted' });
+    // see if there is a better way 4 this
+    const newExperience = {};
+    const experienceValidKeys = Object.keys(
+      Profile.schema.paths.experience.schema.paths
+    ).slice(0, -1);
+    experienceValidKeys.forEach(key => {
+      if (req.body[key]) newExperience[key] = req.body[key];
+    });
+    tryOrServerError(res, async () => {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile)
+        return res.status(400).json(errorsObject('You must create a profile'));
+
+      profile.experience.unshift(newExperience);
+
+      await profile.save();
+      res.json(profile);
     });
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', authUser, (req, res) => {
+  tryOrServerError(res, async () => {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.experience = profile.experience.filter(
+      exp => exp._id != req.params.exp_id
+    );
+
+    await profile.save();
+    res.json({ profile });
+  });
+});
+
+// SEE IF THERE IS A WAY TO DRY THIS THING
+
+// @route   PUT api/profile/education
+// @desc    add profile education to user
+// @access  Private
+router.put(
+  '/education',
+  [
+    authUser,
+    check('school', 'School is required').notEmpty(),
+    check('degree', 'Degree is required').notEmpty(),
+    check('fieldOfStudy', 'Field of study is required').notEmpty(),
+    check('from', 'From is required').notEmpty()
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // see if there is a better way 4 this
+    const newEducation = {};
+    const educationValidKeys = Object.keys(
+      Profile.schema.paths.education.schema.paths
+    ).slice(0, -1);
+    educationValidKeys.forEach(key => {
+      if (req.body[key]) newEducation[key] = req.body[key];
+    });
+    tryOrServerError(res, async () => {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile)
+        return res.status(400).json(errorsObject('You must create a profile'));
+
+      profile.education.unshift(newEducation);
+
+      await profile.save();
+      res.json(profile);
+    });
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', authUser, (req, res) => {
+  tryOrServerError(res, async () => {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education = profile.education.filter(
+      edu => edu._id != req.params.edu_id
+    );
+
+    await profile.save();
+    res.json({ profile });
+  });
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get('/github/:username', (req, res) => {
+  tryOrServerError(res, async () => {
+    const options = {
+      url: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClient'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'get',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    console.log(options);
+    axios(options).then(response => {
+      if (!(response.status >= 200 && response.status <= 300))
+        return res.status(404).json(errorsObject('No github profile found'));
+      res.json(response.data);
+    });
+  });
+});
 
 module.exports = router;
