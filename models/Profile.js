477efb020cf670f2228c66681d88e79d426a3fd6
Maxi-F <maxifeldman@hotmail.com>
2020-03-31 13:17:34 -0300

    forgot to commit, some changes in profile (delete, update, etc)

--- models/Profile.js
+++ models/Profile.js
@@ -1,115 +1,118 @@
 const mongoose = require('mongoose');
 
-const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true
-  },
-  company: {
-    type: String
-  },
-  website: {
-    type: String
-  },
-  location: {
-    type: String
-  },
-  status: {
-    type: String,
-    required: true
-  },
-  skills: {
-    type: [String],
-    required: true
-  },
-  bio: {
-    type: String
-  },
-  githubUsername: {
-    type: String
-  },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
+const ProfileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    company: {
+      type: String
+    },
+    website: {
+      type: String
+    },
+    location: {
+      type: String
+    },
+    status: {
+      type: String,
+      required: true
+    },
+    skills: {
+      type: [String],
+      required: true
+    },
+    bio: {
+      type: String
+    },
+    githubUsername: {
+      type: String
+    },
+    experience: [
+      {
+        title: {
+          type: String,
+          required: true
+        },
+        company: {
+          type: String,
+          required: true
+        },
+        location: {
+          type: String,
+          required: true
+        },
+        from: {
+          type: Date,
+          required: true
+        },
+        to: {
+          type: Date
+        },
+        current: {
+          type: Boolean,
+          default: false
+        },
+        description: {
+          type: String
+        }
+      }
+    ],
+    education: [
+      {
+        school: {
+          type: String,
+          required: true
+        },
+        degree: {
+          type: String,
+          required: true
+        },
+        fieldOfStudy: {
+          type: String,
+          required: true
+        },
+        from: {
+          type: Date,
+          required: true
+        },
+        to: {
+          type: Date
+        },
+        current: {
+          type: Boolean,
+          default: false
+        },
+        description: {
+          type: String
+        }
+      }
+    ],
+    social: {
+      youtube: {
+        type: String
       },
-      company: {
-        type: String,
-        required: true
+      twitter: {
+        type: String
       },
-      location: {
-        type: String,
-        required: true
+      facebook: {
+        type: String
       },
-      from: {
-        type: Date,
-        required: true
+      linkedin: {
+        type: String
       },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
+      instagram: {
         type: String
       }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldOfStudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  social: {
-    youtube: {
-      type: String
     },
-    twitter: {
-      type: String
-    },
-    facebook: {
-      type: String
-    },
-    linkedin: {
-      type: String
-    },
-    instagram: {
-      type: String
+    date: {
+      type: Date,
+      default: Date.now
     }
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { _id: false }
+);
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 const { errorsObject, tryOrServerError } = require('../../logic/logic');
 const authUser = require('../../middleware/auth');
 
@@ -55,10 +56,73 @@ router.post(
       const savedProfile = await Profile.findOneAndUpdate(
         { user: req.user.id },
         { $set: profile },
-        { new: true, useFindAndModify: false, upsert: true }
+        { new: true, upsert: true }
       );
 
       return res.json(savedProfile);
+    });
+  }
+);
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', (req, res) => {
+  tryOrServerError(res, async () => {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  });
+});
+
+// @route   GET api/profile/:user_id
+// @desc    Get profile from user ID
+// @access  Public
+router.get('/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json(errorsObject('no user found'));
+
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    if (error.kind === 'ObjectId')
+      return res.status(400).json(errorsObject('no user found'));
+    return res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete('/', authUser, (req, res) => {
+  tryOrServerError(res, async () => {
+    // @todo remove users posts
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  });
+});
+
+// @route   PUT api/profile/experience
+// @desc    add profile experience to user
+// @access  Private
+router.put(
+  '/experience',
+  [
+    authUser,
+    check('title', 'Title is required').notEmpty(),
+    check('company', 'Company is required').notEmpty(),
+    check('from', 'From is required').notEmpty(),
+    check('location', 'Location is required').notEmpty()
+  ],
+  (req, res) => {
+    tryOrServerError(res, async () => {
+      const experience = new Profile(req.body);
+
+      res.json({ msg: 'User deleted' });
     });
   }
 );
