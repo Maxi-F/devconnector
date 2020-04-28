f62d17fc314ba740f5cc55b017a93a59202db1d6
Maxi-F <maxifeldman@hotmail.com>
2020-04-28 17:01:13 -0300

    finished user controller tests

--- tests/controllerTests/user.test.js
+++ tests/controllerTests/user.test.js
@@ -11,6 +11,11 @@ describe('User Controllers', () => {
     date: 1,
   };
 
+  const userCredentials = {
+    email: 'johnDoe@gmail.com',
+    password: '12345678',
+  };
+
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -30,7 +35,7 @@ describe('User Controllers', () => {
   it('registers an OK user', async () => {
     let savedUser = await registerUser(mockedUser);
 
-    expect(savedUser.errors).toBeFalsy();
+    expect(savedUser.email).toBe(mockedUser.email);
   });
 
   it('does not register an user with validation errors', async () => {
@@ -46,5 +51,28 @@ describe('User Controllers', () => {
     let savedAgainUser = await registerUser(mockedUser);
 
     expect(savedAgainUser.errors[0].msg).toBe('User already in DB');
+  });
+
+  it('logs on an already registered user', async () => {
+    await registerUser(mockedUser);
+
+    const userInDB = await logInUser(userCredentials);
+
+    expect(userInDB.email).toBe(mockedUser.email);
+  });
+
+  it('does not log in a non existant user', async () => {
+    const userInDB = await logInUser(userCredentials);
+
+    expect(userInDB.errors[0].msg).toBe('Invalid Credentials');
+  });
+
+  it('does not log in an user with wrong password', async () => {
+    await registerUser(mockedUser);
+    let badCredentials = { ...userCredentials };
+    badCredentials.password = '1234';
+    const userInDB = await logInUser(badCredentials);
+
+    expect(userInDB.errors[0].msg).toBe('Invalid Credentials');
   });
 });
